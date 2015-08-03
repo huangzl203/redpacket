215050d5c6eb34991f0ee02334f0af0be28aec18
huangzl203 <272950754@qq.com>
2015-08-03 21:20:30 +0800

    提交
    2015年8月3日 21:20:28

--- redpack-web/src/main/java/com/redpack/web/view/account/controller/LoginController.java
+++ redpack-web/src/main/java/com/redpack/web/view/account/controller/LoginController.java
@@ -7,20 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import net.sf.json.JSONObject;
+
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.redpack.common.account.IUserInfoService;
 import com.redpack.common.account.IUserService;
 import com.redpack.common.account.model.UserDo;
-import com.redpack.utils.CommonUtils;
+import com.redpack.common.account.model.UserInfoDo;
 import com.redpack.utils.ResponseUtils;
 import com.redpack.web.view.base.controller.BaseController;
 import comredpack.common.constant.WebConstants;
@@ -41,7 +42,8 @@ public class LoginController extends BaseController {
 
 	@Autowired
 	private IUserService userService;
-	
+	@Autowired
+	private IUserInfoService userInfoService;
 
 	// 登录页验证码标识
 	private final static String pageId = "userlogin";
@@ -50,24 +52,22 @@ public class LoginController extends BaseController {
 	 * 登录入口
 	 * 
 	 * @return
-	 * @author:  zhangyunhua
+	 * @author: zhangyunhua
 	 * @date 2015-3-29 上午3:36:11
 	 */
 	@RequestMapping(value = "index")
-	public String index(String fromUrl, ModelMap map, HttpSession sessionS) {
-		logger.info("----loginInit(初始化登录页面);fromUrl=" + fromUrl + ";----");
-		map.put("fromUrl", fromUrl);
-		logger.debug("fromUrl:{}" + fromUrl);
+	public String index() {
+		logger.info("----loginInit(初始化登录页面);----");
 		int loginStrategy = super.getSessionIntAttr("loginStrategy", 0);
 		request.setAttribute("loginStrategyInfo", loginStrategy >= 5);
 		return "login/login";
 	}
-	
+
 	/**
 	 * 登录入口
 	 * 
 	 * @return
-	 * @author:  zhangyunhua
+	 * @author: zhangyunhua
 	 * @date 2015-3-29 上午3:36:11
 	 */
 	@RequestMapping(value = "main")
@@ -83,57 +83,56 @@ public class LoginController extends BaseController {
 	 * @return 1用户名或密码未输入|2验证码输入错误
 	 */
 	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public void login(HttpServletRequest request,HttpServletResponse response) {
+	public void login(HttpServletRequest request, HttpServletResponse response) {
 		JSONObject jsonObject = new JSONObject();
 		Map<String, Object> parameterMap = new HashMap<String, Object>();
 		String loginInfo = request.getParameter("userName");
 		loginInfo = loginInfo.replaceAll(" ", "");
-		parameterMap.put("username", loginInfo);
+		parameterMap.put("userName", loginInfo);
 		String password = request.getParameter("password");
 		String code = request.getParameter("code");
-		
-		//是否输入用户名密码
-		if (StringUtils.isBlank(loginInfo) || StringUtils.isBlank(password)){
-			 jsonObject.put("result", 1);
-			 ResponseUtils.renderText(response, null, jsonObject.toString());
-			 return;
+
+		// 是否输入用户名密码
+		if (StringUtils.isBlank(loginInfo) || StringUtils.isBlank(password)) {
+			jsonObject.put("result", 1);
+			ResponseUtils.renderText(response, null, jsonObject.toString());
+			return;
 		}
-		
+
 		int loginStrategy = super.getSessionIntAttr("loginStrategy", 0);
-		//密码输入错误5次要求输入验证码
-		if(loginStrategy >=5) {
-			//验证码是否正确
+		// 密码输入错误5次要求输入验证码
+		if (loginStrategy >= 5) {
+			// 验证码是否正确
 			String sessionCode = (String) request.getSession().getAttribute(pageId + "_checkCode");
-			if(StringUtils.isBlank(code) || !code.equals(sessionCode)){
+			if (StringUtils.isBlank(code) || !code.equals(sessionCode)) {
 				jsonObject.put("result", 2);
-				ResponseUtils.renderText(response, null,jsonObject.toString());
+				ResponseUtils.renderText(response, null, jsonObject.toString());
 				return;
 			}
 		}
-		
-		
-		//获取登录用户userId
-//		UserDo loginUser = userService.getByUserDo(parameterMap);
-		//存储登录渠道
+
+		// 获取登录用户userId
+		UserDo loginUser = userService.getByUserDo(parameterMap);
+
 		String pwdMd5 = DigestUtils.md5Hex(password + WebConstants.PASS_KEY);
-		
-		
-		/*if(loginUser == null || !loginUser.getPassword().equals(pwdMd5)) {
+
+		if (loginUser == null || !loginUser.getPassword().equals(pwdMd5)) {
 			// 用户名密码错误
 			jsonObject.put("result", 3);
 			request.getSession().setAttribute("loginStrategy", ++loginStrategy);
 			jsonObject.put("loginStrategy", loginStrategy >= 5);
-			ResponseUtils.renderText(response, null,jsonObject.toString());
+			ResponseUtils.renderText(response, null, jsonObject.toString());
 			return;
-		}*/
-		
-//		request.getSession().setAttribute(WebConstants.SESSION_USER, loginUser);
-//		WebThreadVariable.setUserDo(loginUser);
-		
+		}
+		UserInfoDo userInfoDo = userInfoService.getByUserId(loginUser.getId());
+		loginUser.setUserInfoDo(userInfoDo);
+		request.getSession().setAttribute(WebConstants.SESSION_USER, loginUser);
+		WebThreadVariable.setUserDo(loginUser);
+
 		// 取缓存登录信息
-		String fromUrl = request.getHeader("referer");
+		// String fromUrl = request.getHeader("referer");
 		jsonObject.put("result", 0);
-		jsonObject.put("fromUrl", fromUrl);
+		jsonObject.put("fromUrl", "/redPack/personalCenter.html");
 		// 登陆成功
 		ResponseUtils.renderText(response, "UTF-8", jsonObject.toString());
 	}
@@ -151,6 +150,5 @@ public class LoginController extends BaseController {
 		request.getSession().invalidate();
 		return "redirect:/login/index.do";
 	}
-	
-	
+
 }
